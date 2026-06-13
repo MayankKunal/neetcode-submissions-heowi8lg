1d63bee2148293c419dd17868262617216b237e5
MAYANK KUMAR <89400788+MayankKunal@users.noreply.github.com>
2026-06-13 12:41:16 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int n=prices.size();
+        int profit=0;
+        int cp=prices[0];
+        for(int i=0;i<n;i++)
+        {
+            cp=min(cp,prices[i]);
+            profit=max(profit,prices[i]-cp);
+        }
+        return profit;
+    }
+};
